eaf1ab21986b8b8c4cc705604277de27df446873
SachinDeosarkar <deosarkar.sachin1@gmail.com>
2023-09-04 12:27:48 +0530

    My Commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+  
+resource "aws_instance" "MyEC2" {
+  ami           = "ami-0ded8326293d3201b" 
+  instance_type = "t2.micro"
+  
+}
