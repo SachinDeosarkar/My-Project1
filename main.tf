f3c5c3091073660e5bd010223d446d63385be3d3
SachinDeosarkar <deosarkar.sachin1@gmail.com>
2023-09-04 12:47:30 +0530

    Ec2 Name change

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "ap-south-1"
 }
   
-resource "aws_instance" "MyEC2" {
+resource "aws_instance" "EC2ByTerraformjenkins" {
   ami           = "ami-0ded8326293d3201b" 
   instance_type = "t2.micro"
   
